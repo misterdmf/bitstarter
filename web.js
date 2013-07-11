b1132f54d5c442abef863e6d43394adb22e633df
Davi Morais <misterdmf@gmail.com>
2013-07-11 05:39:18 +0000

    Just one teste

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var file = "index.html";
+var content = fs.readFileSync(file);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worrrrrld 2!');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
